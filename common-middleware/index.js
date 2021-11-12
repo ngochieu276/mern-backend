24dad23d899a7c2e49f3276e1004201c85544f54
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2021-11-12 21:08:34 +0700

    rename update route, fix token auth

--- common-middleware/index.js
+++ common-middleware/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 exports.requireSignin = (req, res, next) => {
   if (req.headers.authorization) {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization;
     const user = jwt.verify(token, process.env.JWT_SECRET);
     console.log(user);
     req.user = user;
