0c03a7935cc746c082c2b99f4e806ce775a20fe3
hieu <ngochieu2761998@gmail.com>
2021-12-08 10:25:47 +0700

    fix order schema

--- models/product.js
+++ models/product.js
@@ -36,7 +36,7 @@ const productSchema = new mongoose.Schema(
     },
     profilePicture: { type: String },
     createdAt: { type: Date },
-    createBy: {
+    createdBy: {
       type: mongoose.Schema.ObjectId,
       ref: "User",
       required: true,
